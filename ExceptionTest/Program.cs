9e451b4377db0caaa9e1a6548e9525736011703c
Insoel <insoel@gmail.com>
2018-11-06 10:19:04 +0000

    Add a try and catch

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -10,9 +10,18 @@ namespace ExceptionTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Insere um numero inteiro: ");
-            int i = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Numero inserido: {i}");
+            try
+            {
+                Console.WriteLine("Insere um numero inteiro: ");
+                int i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Numero inserido: {i}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ocorreu o seguinte problema: " + e.Message);
+            }
+
+
         }
     }
 }
